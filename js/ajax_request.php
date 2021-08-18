a785357c05929c0dd8de0844eba1ae689e0397fe
umi777 <57103886+umi777@users.noreply.github.com>
2021-08-18 22:30:16 +0700

    Add files via upload

--- js/ajax_request.php
+++ js/ajax_request.php
@@ -5,16 +5,16 @@ $db = new database();
 $username 	= 'umi777';
 $tblname = 'phonebook';
 
-$list = $db->listContact($tblname);
-foreach ($list as $arr) {
-	//echo("$arr[name] $arr[phone] ");
-	echo "\n",'<div class="contact">',"\n";
-	echo "\n",'<span class="name">'.($arr[name]).'</span>';
-	echo "\n",'<span class="del" onclick="ajaxRequest('.$arr[ID].')">x</span>';
-	echo "\n",'<br>';
-	echo "\n",'<span class="phone">'.$arr[phone].'</span>';
-	echo "\n",'</div>';
-	}
+					$list = $db->listContact($tblname);
+					foreach ($list as $arr) {
+						//echo("$arr[name] $arr[phone] \n");
+						echo '<div class="contact">';
+						echo '<span class="name">'.($arr[name]).'</span>';
+						echo '<span class="del" onclick="ajaxRequest('.$arr[ID].')">x</span>';
+						echo '<br>';
+						echo '<span class="phone">'.$arr[phone].'</span>';
+						echo '</div>';
+						}
 }
 if (isset($_GET['id'])) {
 include_once('../db.php');
