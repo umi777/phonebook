d4c5810a270286ba2546dc6c4d111e3a673c2b7c
umi777 <57103886+umi777@users.noreply.github.com>
2021-08-18 22:31:15 +0700

    Add files via upload

---
+++ db.php
@@ -0,0 +1,105 @@
+<?php
+
+class database {
+	
+	private $host 		= 'mysql.zzz.com.ua';
+    private $dbname 	= 'umi9956';
+    private $username 	= 'umi777';
+    private $password 	= '000000Az';
+    private $charset 	= 'utf8';
+	private $mysqli;
+	
+	function __construct () {
+
+		$this->mysqli = @new PDO('mysql:host='.$this->host.';dbname='.$this->dbname,$this->username,$this->password);
+
+	}
+
+	
+	public function Connect ($user) {
+		// Type your code here
+	    $mysqli = @new mysqli($this->host, $user, $this->password, $this->dbname);
+		if ($mysqli->connect_errno) {
+        return "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+		} else {
+		return  $mysqli;
+		}
+		
+	}
+	
+	function CheckTable ($TableName) {
+		$dbh = $this->mysqli;
+		//echo $TableName."<br>";
+		$sql = $dbh->prepare('SHOW TABLES LIKE ?');
+        $sql->execute(array($TableName));
+		$res = $sql->fetchAll();
+		//var_dump($res);echo "-res<br>";
+	return ($res ?? false);
+	}
+	
+	function CreateTable ($dbname,$TableName) {
+		$dbh = $this->mysqli;
+		echo $TableName;
+        $res = $dbh->query("CREATE TABLE $dbname.$TableName ( `ID` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(50) NOT NULL , `phone` VARCHAR(25) NOT NULL , PRIMARY KEY (`ID`), INDEX `name` (`name`)) ENGINE = MyISAM CHARSET=utf8 COLLATE utf8_general_ci;");
+		var_dump($res);
+	return ($res);
+
+	}
+	function addContact($tblname, $post) {
+		$dbh = $this->mysqli;
+			//echo $tblname."<br>";	
+			//print_r ($post);	
+		if ($this->CheckTable($tblname)) {
+			$sql  = $dbh->prepare("INSERT INTO $tblname (name,phone) VALUES (?,?)");
+			
+			if ($sql->execute(array (htmlspecialchars($post['name']), htmlspecialchars($post['phone'])))) { 
+				return ("<script>document.getElementById(\"list_footer\").innerHTML = \"Добавлен контакт ".$post['name']."\";</script>");
+			} else {
+				return ("Error updating record: " . $dbh->error);
+			}	
+		} else {
+			$this->CreateTable($this->$dbname,$tblname);
+			$this->addContact($tblname, $post);
+		}
+	}
+
+	function delContact($tblname, $id) {
+		$dbh = $this->mysqli;
+		$sql = ($dbh->prepare('SELECT `name` FROM '.$tblname.' WHERE '.$tblname.'.`ID` = ?'));
+		//echo "<br>";
+		//var_dump ($res,$sql,$id);
+		//echo "<br>";
+		if ($sql->execute(array($id))) { 
+				$res=$sql->fetchAll(PDO::FETCH_ASSOC);
+				$sql = ($dbh->prepare("DELETE FROM $tblname WHERE $tblname.`ID` = ?"));
+				$sql->execute(array($id));
+				return ($res);
+			} else {
+			    return ($sql->fetchAll());
+			}
+		}
+
+	function listContact ($tblname) {
+
+		if ($this->CheckTable($tblname)) {
+			$dbh = $this->mysqli;
+			$sql  = $dbh->prepare('SELECT * FROM '.$tblname);
+			if ($sql->execute()) {
+				while ($row = $sql->fetch(PDO::FETCH_ASSOC)) {
+				$res[] = ($row);
+				//echo "<br>";
+				}
+				return($res);
+			}/*
+			if ($dbh->prepare($sql) === TRUE) {
+				return ($dbh->query($sql));
+			} else {
+				return ("Error updating record: " . $dbh->error);
+			}	*/
+		} else {
+			return ("Таблица ".$tblname." не найдена!");
+		} 
+			
+	}
+
+}
---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+
+include_once(__DIR__ . '/db.php');
+$db = new database();
+$username 	= 'umi777';
+$tblname = 'phonebook';
+
+if (isset($_POST[delete])) {echo "Был удален ";
+ ($db->delContact($tblname ,$_POST[delete]));
+echo " контакт";}
+
+?>
+	
+<!DOCTYPE html>
+<html dir="ltr" lang="ru" class="smallTop">
+<head>
+
+	<title>Телефонная книга</title>
+	<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/style.css?v=1.020" />
+	<script type="text/javascript" src="js/js.js"></script>
+</head>
+<body>
+	<div class="wrapper">
+		<div>
+	<div class="" id="form_add">
+		<div class="header" id="">
+			<h1>Добавить&nbsp;контакт</h1>
+		</div>
+		<form id="add_phone" name="myform" action="" method="post">
+		<fieldset>
+				<div class="name" id="name">
+					<input placeholder="Имя" type="text" name="name" value="<?=($_POST['name']?? false)?>" required />
+				</div>
+				<div class="phone" id="phone">
+					<input placeholder="+79876543210" type="phone" name="phone"  value="<?=($_POST['phone']?? false)?>" required />
+				</div>
+				<div class="submit" id="submit">
+					<input type="submit" name="add_phone" value="Добавить" />
+					
+				</div>
+		</fieldset>
+	</form>
+	</div>
+	<div class="" id="form_contact">
+		<div class="header" id="">
+			<h1>Список контактов</h1>
+		</div>
+		<div class="" id="list_contact">
+			<!--<form action="" method="post"> -->
+				<?php 
+					$list = $db->listContact($tblname);
+					foreach ($list as $arr) {
+						echo '<div class="contact">';
+						echo '<span class="name">'.($arr[name]).'</span>';
+						echo '<span class="del" onclick="ajaxRequest('.$arr[ID].')">x</span>';
+						echo '<br>';
+						echo '<span class="phone">'.$arr[phone].'</span>';
+						echo '</div>';
+						}
+					
+				?>
+			<!--</form>-->
+		</div>
+		<div id="list_footer"></div>
+	</div>
+	</div>
+	</div>
+</body>
+</html>
+<?php 
+if (isset($_POST[add_phone])) {
+	echo ($db->addContact($tblname ,$_POST));
+	echo ("<script>ajaxRequest('list');</script>");
+}
+?>
